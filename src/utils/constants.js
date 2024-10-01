12320f1b7b42143e275c47f5d1f4847dcfd50ded
Himaja9876 <himajachinnam8@gmail.com>
2024-09-30 23:29:38 -0400

    modified constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,14 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 
 export const AVATARLOGO = "https://i.pinimg.com/550x/e3/94/30/e39430434d2b8207188f880ac66c6411.jpg"
 
-
+export const OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDU1OWY0NWE0OWYwNTNkZDkxYmU3YWEzZDFjODU2NSIsIm5iZiI6MTcyNDQyMTU1Ni42NTg2NTQsInN1YiI6IjY2Yzg3YmRmMzE3MTVhM2JjNmEzM2M5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dPBINd1RFcqcTkDRdlLLvY3Y946FJNAPtVDEbmkma9k'
+    }
+  };
+  
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/"
 
 export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
