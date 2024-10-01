bf7dbd8c7d11c294431c445698ee8ea669b2e008
Himaja9876 <himajachinnam8@gmail.com>
2024-10-01 17:06:15 -0400

    GPT Search Page with Movie Suggestions

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDU1OWY0NWE0OWYwNTNkZDkxYmU3YWEzZDFjODU2NSIsIm5iZiI6MTcyNDQyMTU1Ni42NTg2NTQsInN1YiI6IjY2Yzg3YmRmMzE3MTVhM2JjNmEzM2M5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dPBINd1RFcqcTkDRdlLLvY3Y946FJNAPtVDEbmkma9k'
+      Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
     }
   };
   
@@ -20,3 +20,4 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
+export const OPEN_API_KEY = process.env.REACT_APP_OPEN_API_KEY;
